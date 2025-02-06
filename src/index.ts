4b1873681db4f232c7f722c34e4c3f0ca86ef85d
Alexandre Annic <alexandre.annic1@gmail.com>
2025-02-06 08:16:50 -0500

    Draft simple index.js

--- src/index.ts
+++ src/index.ts
@@ -1,34 +1,22 @@
 import {migrate} from './migrate.js'
-import {z} from 'zod'
-import dotenv from 'dotenv'
 
-dotenv.config()
-
-const envSchema = z.object({
-  KOBO_SOURCE_URL: z.string(),
-  KOBO_SOURCE_URLV1: z.string(),
-  KOBO_SOURCE_TOKEN: z.string(),
-  KOBO_DESTINATION_URL: z.string(),
-  KOBO_DESTINATION_URLV1: z.string(),
-  KOBO_DESTINATION_TOKEN: z.string(),
-})
-const env = envSchema.parse(process.env)
-
-  await migrate({
-    dryRun: true,
-    filters: {
-      submissionDateStart: new Date(2025, 0, 12),
-      // formIdsIgnored: ['aLEGqicGyzkZCeCYeWqEyG', 'aJaGLvGEdpYWk5ift8k87y'],
-      // formIds: ['aoJppKLX7QvSkMYokUfEjB'],
-    },
-    destination: {
-      token: env.KOBO_DESTINATION_TOKEN,
-    urlv1: env.KOBO_DESTINATION_URLV1,
-    urlv2: env.KOBO_DESTINATION_URL,
+await migrate({
+  dryRun: true,
+  filters: {
+    submissionDateStart: new Date(2025, 0, 1),
+    submissionDateEnd: new Date(2025, 1, 1),
+    formIds: ['aoJppKLX7QvSkMYokUfEjB'],
+    offset: 10,
+    take: 1000,
+  },
+  destination: {
+    token: '...',
+    urlv1: 'https://kc-eu.kobotoolbox.org',
+    urlv2: 'https://eu.kobotoolbox.org',
   },
   source: {
-    token: env.KOBO_SOURCE_TOKEN,
-    urlv1: env.KOBO_SOURCE_URLV1,
-    urlv2: env.KOBO_SOURCE_URL,
+    token: '...',
+    urlv1: 'https://kc.kobotoolbox.org',
+    urlv2: 'https://kf.kobotoolbox.org',
   },
 })
